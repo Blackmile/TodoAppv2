273bf94298fb4c0642839ff059e95c97bb06d43f
Blackmile <stanley.ezeifeanyi@gmail.com>
2025-05-25 13:44:44 +0100

    fix app navigation

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/Home';
+import Note from './src/Note';
+import Todo from './src/Todo';
+import { NavigationContainer } from '@react-navigation/native';
+import Foundation from '@expo/vector-icons/Foundation'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator 
+      style={{ paddingTop: 50}}
+      tabBarPosition='top'
+      screenOptions={({route}) => ({
+        tabBarShowLabel: false,
+        tabBarIcon: ({focus, color, size}) => {
+          let rn = route.name;
+
+          if (rn === 'Home'){
+            return <Ionicons name='home-outline' size={20} />
+          }
+
+          if (rn === 'Note'){
+            return <Foundation name='clipboard-notes' size={20} />
+          }
+
+          if (rn === 'Todo'){
+            return <FontAwesome5 name="sticky-note" size={20} color="black" />
+          }
+        }})}
+      >
+        <Tab.Screen name='Home' component={Home} />
+        <Tab.Screen name='Note' component={Note} />
+        <Tab.Screen name='Todo'  component={Todo}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
