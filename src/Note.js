8f44e018594d179e7bdbb0c6d3264350e1dc411f
Blackmile <stanley.ezeifeanyi@gmail.com>
2025-05-26 15:04:05 +0100

    testing flatlist

--- src/Note.js
+++ src/Note.js
@@ -1,10 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const Note = () => {
+
+  const Data = [{
+      id: '1',
+      title: 'First Item',
+    },
+    {
+      id: '2',
+      title: 'Second Item',
+    },
+    {
+      id: '3',
+      title: 'Third Item',
+    },
+    {
+      id: '4',
+      title: 'Third Item',
+    },
+    {
+      id: '5',
+      title: 'Third Item',
+    },
+    {
+      id: '6',
+      title: 'Third Item',
+    },
+  ]
+
   return (
     <View>
-      <Text>Note</Text>
+      <FlatList
+        data={Data}
+        renderItem={({ item }) => (
+          <Text> {item.title} </Text>
+        )}
+      />
     </View>
   )
 }
