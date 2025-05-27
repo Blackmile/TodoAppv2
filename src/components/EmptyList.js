3f1400bc5772d0386e892b60ee1b01681c0bf43f
Blackmile <stanley.ezeifeanyi@gmail.com>
2025-05-27 13:06:50 +0100

    preload screen component

---
+++ src/components/EmptyList.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+const EmptyList = () => {
+  return (
+    <View>
+      <Text>Nothing on your list of Todo</Text>
+    </View>
+  )
+}
+
+export default EmptyList
+
+const styles = StyleSheet.create({})
