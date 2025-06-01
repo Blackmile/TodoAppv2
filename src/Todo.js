cff1d21464c97d899ff3bee0b8ed713ac6f5bb9c
Blackmile <stanley.ezeifeanyi@gmail.com>
2025-06-01 22:03:15 +0100

    fix the flatlist to display last item first, added the finished task component

--- src/Todo.js
+++ src/Todo.js
@@ -1,15 +1,17 @@
-import { FlatList, Pressable, StyleSheet, View } from 'react-native'
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { useState } from 'react'
 import EmptyList from './components/EmptyList';
 import TodoCard from './components/TodoCard';
 import TodoInput from './components/TodoInput';
+import DoneTask from './components/DoneTask';
 
 const Todo = () => {
 
   const [taskItems, setTaskItems] = useState([])
   const [modalVisible, setModalVisible] = useState(false);
-  
+  const [isVisible, setIsVisible] = useState(false)
+  const [count, setCount] = useState([])
   const handleAddTask = (task) => {
       setTaskItems((currTask) => [...currTask, {text: task, id: Math.random().toString()}])
       setModalVisible(!modalVisible)
@@ -21,15 +23,31 @@ const Todo = () => {
     alert('you deleted task')
   }
   
+  const completeTask = (id, text) => {
+    setCount(text)
+    setTaskItems(currTask => (currTask.filter((task) => task.id !== id)))
+    console.log(count, 'i am count')
+  }
+
+  
 
   return (
     <View style={styles.container}>
-      <FlatList ListEmptyComponent={<EmptyList/>} data={taskItems} renderItem={({item}) => (<TodoCard id={item.id} text={item.text} onDelete={deleteTask} onCheck />)} />
-      <View style={styles.addBtn}>
-        <Pressable onPress={()=>{console.log('pressed'), setModalVisible(true)} }>
-          <Ionicons name="add-circle-sharp" size={52} color="white" />
-        </Pressable> 
+      <FlatList ListEmptyComponent={<EmptyList/>} data={[...taskItems].reverse()} renderItem={({item}) => (<TodoCard id={item.id} text={item.text} onDelete={deleteTask} onCheck={completeTask} />)}
+      contentContainerStyle={taskItems.length === 0 && {flex: 1}} />
+      <View style={styles.footer} >
+        <View style={styles.doneTask} >
+          <Pressable onPress={() => {console.log('pressed for dropdown'), setIsVisible(true)}}>
+            <Text>view completed tasks</Text>
+          </Pressable>
+        </View>
+        <View style={styles.addBtn}>
+          <Pressable onPress={()=>{console.log('pressed'), setModalVisible(true)} }>
+            <Ionicons name="add-circle-sharp" size={52} color="white" />
+          </Pressable> 
+        </View>
       </View>
+      <DoneTask onOpen={isVisible} onClose ={() => setIsVisible(false)} numTask={count.length}/>
       <TodoInput onCancel={() => setModalVisible(false)} onDone={handleAddTask} onVisible={modalVisible} onRequest={() => setModalVisible(false)}  />
     </View>
   )
@@ -39,14 +57,21 @@ export default Todo
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'green',
-    display: 'flex',
+    backgroundColor: '#E4E9E9',
     flex: 1,
   },
   addBtn:{
-    position: 'absolute',
-    bottom: 100,
     right: 30,
   },
+  footer: {
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    width: 400,
+    position: 'absolute',
+    bottom: 100,
+    margin: 10,
+  },
+  doneTask: {},
   
 })
