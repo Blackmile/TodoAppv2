a434b837983a75a5e703064b0ce24445e18fb463
Blackmile <stanley.ezeifeanyi@gmail.com>
2025-06-01 22:03:48 +0100

    added style to tabBar

--- App.js
+++ App.js
@@ -10,15 +10,15 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 const Tab = createMaterialTopTabNavigator();
-
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator 
-      style={{ paddingTop: 50}}
+      style={{ paddingTop: 50, backgroundColor: '#E4E9E9'}}
       tabBarPosition='top'
       screenOptions={({route}) => ({
         tabBarShowLabel: false,
+        tabBarStyle: {backgroundColor: '#E4E9E9'},
         tabBarIcon: ({focus, color, size}) => {
           let rn = route.name;
 
