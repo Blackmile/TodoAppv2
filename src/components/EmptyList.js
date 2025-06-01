fabd43c49344e3d553d3cd5e8148806c304326d7
Blackmile <stanley.ezeifeanyi@gmail.com>
2025-06-01 21:57:30 +0100

    added styling

--- src/components/EmptyList.js
+++ src/components/EmptyList.js
@@ -4,12 +4,24 @@ import React from 'react'
 
 const EmptyList = () => {
   return (
-    <View>
-      <Text>Nothing on your list of Todo</Text>
+    <View style={styles.container}>
+      <Text style={styles.textStyle} >All tasks are completed</Text>
     </View>
   )
 }
 
 export default EmptyList
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    bottom: -150,
+  },
+  textStyle: {
+    fontSize: 24,
+    fontWeight: 600,
+    color: '#524F4F',
+  }
+})
