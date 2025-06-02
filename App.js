bd13652aebf2339866fb97c1d5c429cfe464cccb
Blackmile <stanley.ezeifeanyi@gmail.com>
2025-06-02 20:39:34 +0100

    add redux to app

--- App.js
+++ App.js
@@ -8,38 +8,46 @@ import { NavigationContainer } from '@react-navigation/native';
 import Foundation from '@expo/vector-icons/Foundation'
 import Ionicons from '@expo/vector-icons/Ionicons';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { Provider } from 'react-redux';
+import { store, persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const Tab = createMaterialTopTabNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator 
-      style={{ paddingTop: 50, backgroundColor: '#E4E9E9'}}
-      tabBarPosition='top'
-      screenOptions={({route}) => ({
-        tabBarShowLabel: false,
-        tabBarStyle: {backgroundColor: '#E4E9E9'},
-        tabBarIcon: ({focus, color, size}) => {
-          let rn = route.name;
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
 
-          if (rn === 'Home'){
-            return <Ionicons name='home-outline' size={20} />
-          }
+        <NavigationContainer>
+          <Tab.Navigator 
+          style={{ paddingTop: 50, backgroundColor: '#E4E9E9'}}
+          tabBarPosition='top'
+          screenOptions={({route}) => ({
+            tabBarShowLabel: false,
+            tabBarStyle: {backgroundColor: '#E4E9E9'},
+            tabBarIcon: ({focus, color, size}) => {
+              let rn = route.name;
 
-          if (rn === 'Note'){
-            return <Foundation name='clipboard-notes' size={20} />
-          }
+              if (rn === 'Home'){
+                return <Ionicons name='home-outline' size={20} />
+              }
 
-          if (rn === 'Todo'){
-            return <FontAwesome5 name="sticky-note" size={20} color="black" />
-          }
-        }})}
-      >
-        <Tab.Screen name='Home' component={Home} />
-        <Tab.Screen name='Note' component={Note} />
-        <Tab.Screen name='Todo'  component={Todo}/>
-      </Tab.Navigator>
-    </NavigationContainer>
+              if (rn === 'Note'){
+                return <Foundation name='clipboard-notes' size={20} />
+              }
+
+              if (rn === 'Todo'){
+                return <FontAwesome5 name="sticky-note" size={20} color="black" />
+              }
+            }})}
+          >
+            <Tab.Screen name='Home' component={Home} />
+            <Tab.Screen name='Note' component={Note} />
+            <Tab.Screen name='Todo'  component={Todo}/>
+          </Tab.Navigator>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
 
