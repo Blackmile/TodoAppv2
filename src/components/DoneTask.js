828b0e1e2d82cb3ff0a251228a314b542ac4ba86
Blackmile <stanley.ezeifeanyi@gmail.com>
2025-06-03 00:06:13 +0100

    clean up and rendered data to the flatlist

--- src/components/DoneTask.js
+++ src/components/DoneTask.js
@@ -1,46 +1,10 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native'
 import Modal from 'react-native-modal';
+import { useSelector, useDispatch } from 'react-redux';
 
 const DoneTask = (props) => {
 
-    const DATA = [
-        {
-            id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-            title: 'First Item',
-        },
-        {
-            id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-            title: 'Second Item',
-        },
-        {
-            id: '58694a0f-3da1-471f-bd96-145571e29d72',
-            title: 'Third Item',
-        },
-        {
-            id: '1',
-            title: 'First Item',
-        },
-        {
-            id: '2',
-            title: 'Second Item',
-        },
-        {
-            id: '3',
-            title: 'Third Item',
-        },
-        {
-            id: '4',
-            title: 'Third Item',
-        },
-        {
-            id: '5',
-            title: 'Third Item',
-        },
-        {
-            id: '6',
-            title: 'Third Item',
-        },
-    ];
+    const DATA = useSelector(state => state.doneTodos.doneTodos)
     
 
   return (
@@ -55,10 +19,10 @@ const DoneTask = (props) => {
         backdropTransitionOutTiming={0}
       >
         <View style={styles.content}>
-          <Text style={styles.textStyle} > completed task {props.numTask} </Text>
+          <Text style={styles.textStyle} > completed task {DATA.length} </Text>
           <FlatList 
             data={DATA} 
-            renderItem={({item}) => <Text>{item.title}</Text>}
+            renderItem={({item}) => <Text>{item.text}</Text>}
             initialNumToRender={5}
           />
         </View>
