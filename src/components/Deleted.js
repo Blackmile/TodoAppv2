ffde096e2b2059195fc1a1837244a442a90e97e2
Blackmile <stanley.ezeifeanyi@gmail.com>
2025-06-11 00:10:07 +0100

    added the card component and implemented the recover btn

--- src/components/Deleted.js
+++ src/components/Deleted.js
@@ -1,33 +1,63 @@
 import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { useDispatch, useSelector } from 'react-redux'
-import { emptyList } from '../../slices/recoverSlice'
+import { deleteItem, emptyList } from '../../slices/recoverSlice'
+import { addTodo } from '../../slices/todoSlice'
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Deleted = () => {
     const navigation = useNavigation()
 
-    const data = [{id: 1, text: 'first'}]
-
     const dispatch = useDispatch()
 
-    const delItem = useSelector(state => state.recoverTodo.recoverTodo)
+    const data = useSelector(state => state.recoverTodo.recoverTodo)
 
-    console.log(delItem)
+    const returnTodo = (id, text) => {
+      dispatch(addTodo(text))
+      dispatch(deleteItem(id))
+    }
+
+    console.log(data)
+
+    const Card = (props) => {
+      return(
+        <View style={styles.card}>
+          <Text> {props.text} </Text>
+          <Pressable 
+            onPress={()=> {
+              props.onRecover(props.id, props.text)
+            }} style={styles.recoverBtn} >
+            <Ionicons name="arrow-undo" size={16} color="black" />
+            <Text> Recover </Text>
+          </Pressable>
+        </View>
+      )
+    }
 
   return (
     <View style={styles.container} >
-      <Text> display deleted items with options to recover </Text>
-      <Text> Recover all task </Text>
-      <Pressable onPress={() => dispatch(emptyList()) } style={{padding: 20, backgroundColor: 'red'}} >
-        <Text> empty trash </Text>
-      </Pressable>
-      <Pressable onPress={() => navigation.goBack()} >
-        <Text>go back</Text>
-      </Pressable>
+      <View style={styles.header} >
+        <Pressable onPress={() => navigation.goBack()} style={styles.back} >
+          <Ionicons name="arrow-back-circle-sharp" size={22} color="black" />
+          <Text>back</Text>
+        </Pressable>
+        <Text style={styles.title} > Recently Deleted </Text>
 
-        <FlatList data={delItem} renderItem={({item}) => (<Text> {item.text} </Text>) } />
+      </View>
 
+      <FlatList style={styles.listStyle} data={data} renderItem={({item}) => (<Card id={item.id} text={item.text} onRecover={returnTodo} />) } />
+
+      
+      <View style={styles.footer} >
+        <Pressable style={styles.back} onPress={()=> console.log('retun!!')} >
+          <Ionicons name="arrow-undo" size={24} color="black" />
+          <Text> Recover all task </Text>
+        </Pressable>
+        <Pressable onPress={() => dispatch(emptyList()) } style={styles.back} >
+          <Ionicons name="trash-sharp" size={24} color="black" />
+          <Text> empty trash </Text>
+        </Pressable>
+      </View>
     </View>
   )
 }
@@ -38,6 +68,44 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       justifyContent: 'center',
-      alignContent: 'center',  
+      alignContent: 'center',
+      backgroundColor: '#E4E9E9',  
     },
+    header: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    title: {
+      paddingLeft: 50,
+      fontSize: 28,
+      fontWeight: 600,
+    },
+    back: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      left: 6,
+    },
+    footer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: 50, 
+    },
+    listStyle: {
+      marginTop: 20,
+    },
+    card: {
+      padding: 5,
+      backgroundColor: '#fff',
+      marginBottom: 10,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignContent: 'center',
+      alignItems: 'center',
+      borderRadius: 8,
+    },
+    recoverBtn: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    }
 })
